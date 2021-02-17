bfa2c720b4aa97f4c89782d5c41e6acff6801a93
kevin <kevin.melan00@usc.edu.co>
2021-02-16 22:46:27 -0500

    Change enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  // API_DOMAIN: 'http://pokemonapi-env.eba-mpv5tqkp.us-east-2.elasticbeanstalk.com/api/v1',
-  API_DOMAIN: 'http://localhost:8080/api/v1',
-  API_DOMAIN_SOCKETS: 'http://localhost:8080',
-  // API_DOMAIN_SOCKETS: 'http://pokemonapi-env.eba-mpv5tqkp.us-east-2.elasticbeanstalk.com'
+  API_DOMAIN: 'http://pokemonapi-env.eba-mpv5tqkp.us-east-2.elasticbeanstalk.com/api/v1',
+  // API_DOMAIN: 'http://localhost:8080/api/v1',
+  // API_DOMAIN_SOCKETS: 'http://localhost:8080',
+  API_DOMAIN_SOCKETS: 'http://pokemonapi-env.eba-mpv5tqkp.us-east-2.elasticbeanstalk.com'
 };
 
 /*
